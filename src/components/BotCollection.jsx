5ce5629e5b95a33fae8c1849ab8f50d50cb57c10
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-18 17:23:52 +0300

    import BotCard in jsx

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,13 +1,23 @@
-export default function BotCollection({ bots, onEnlist }) {
+import BotCard from "./BotCard";
+
+export default function BotCollection({
+  bots,
+  showBotSpecs,
+  enlistBot,
+  dischargeBot,
+}) {
   return (
-    <div>
-      <h2>Bot Collection</h2>
-      <div className="bot-list">
+    <div className="container mb-4">
+      <h2 className="mb-4">Bot Collection</h2>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5 g-3">
         {bots.map((bot) => (
-          <div key={bot.id} onClick={() => onEnlist(bot)}>
-            <img src={bot.avatar_url} alt={bot.name} />
-            <h3>{bot.name}</h3>
-            <p>{bot.catchphrase}</p>
+          <div className="col" key={bot.id}>
+            <BotCard
+              bot={bot}
+              showBotSpecs={showBotSpecs}
+              enlistBot={enlistBot}
+              dischargeBot={dischargeBot}
+            />
           </div>
         ))}
       </div>
